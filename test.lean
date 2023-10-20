f470ebb1ec6d67c868ca1a9a7fd018133c30de8e
Florent Schaffhauser <7537413+matematiflo@users.noreply.github.com>
2023-10-20 18:45:55 +0000

    Added more examples

--- test.lean
+++ test.lean
@@ -1,3 +1,7 @@
 import Mathlib.Algebra.Group.Defs
 
-example (G : Type) [Group G] : 1 * 1 = 1 := by {rfl} 
+example (G : Type) [Group G] : 1 * 1 = 1 := by {rfl}
+
+example : 1 + 1 = 2 := refl (1 + 1)
+example : 1 + 1 = 2 := refl 2
+example : 1 + 1 = 2 := rfl
