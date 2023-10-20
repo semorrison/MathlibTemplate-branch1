d3bdb2bf12ad96d37977f5450c8c55ce8292b3e0
Florent Schaffhauser <7537413+matematiflo@users.noreply.github.com>
2023-10-20 18:49:25 +0000

    Added explanation on rfl

--- test.lean
+++ test.lean
@@ -4,4 +4,4 @@ example (G : Type) [Group G] : 1 * 1 = 1 := by {rfl}
 
 example : 1 + 1 = 2 := refl (1 + 1)
 example : 1 + 1 = 2 := refl 2
-example : 1 + 1 = 2 := rfl
+example : 1 + 1 = 2 := rfl -- same as `refl` but with the argument taken implicitly
